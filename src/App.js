6f22786c130181d2d5b3537c0b6ba2080fc837b9
Joker-Bat <shanmugam091098@gmail.com>
2022-02-17 11:32:29 +0530

    Testing otp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+import { autoReadSMS } from "./AutoReadSMS";
 
 function App() {
+  const [otp, setOtp] = useState("");
+
+  const handleGetOtp = () => {
+    autoReadSMS(setOtp);
+  };
+
+  console.log(otp);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Received otp</h1>
+      <br />
+      <h1>{otp}</h1>
+
+      <button onClick={handleGetOtp}>Get otp</button>
     </div>
   );
 }
---
+++ src/AutoReadSMS.js
@@ -0,0 +1,34 @@
+export function autoReadSMS(cb) {
+  // used AbortController with setTimeout so that WebOTP API (Autoread sms) will get disabled after 1min
+  console.log("Called: autoReadSMS");
+  const signal = new AbortController();
+  setTimeout(() => {
+    signal.abort();
+  }, 1 * 60 * 1000);
+  async function main() {
+    console.log("Called: main");
+    if ("OTPCredential" in window) {
+      try {
+        if (navigator.credentials) {
+          try {
+            await navigator.credentials
+              .get({ abort: signal, otp: { transport: ["sms"] } })
+              .then((content) => {
+                console.log("Content:", content);
+                if (content && content.code) {
+                  cb(content.code);
+                }
+              })
+              .catch((e) => console.log(e));
+          } catch (err) {
+            console.log("err ", err);
+            return;
+          }
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  main();
+}
